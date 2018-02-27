56f55ec2033a259849114799b826a94e45ca42a1
Dan Hou <31412607+doradhou@users.noreply.github.com>
2018-02-26 23:53:10 -0500

    Add course model

--- Team_CourseAnalyses/courses/models.py
+++ Team_CourseAnalyses/courses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    course_id = models.CharField(max_length=5)
+    course_title = models.CharField(max_length=100)
+    session = models.CharField(max_length=50)
+    units = models.CharField(max_length=10)
+    section = models.CharField(max_length=5)
+    mini = models.CharField(max_length=10)
+    days = models.CharField(max_length=10)
+    begin = models.CharField(max_length=10)
+    end = models.CharField(max_length=10)
+    teaching_location = models.CharField(max_length=50)
+    bldg_room = models.CharField(max_length=50)
+    instructor = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.course_title
