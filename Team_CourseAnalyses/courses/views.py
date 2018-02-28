5fba733681b0658129088684c42b68fba642d96a
Dan Hou <31412607+doradhou@users.noreply.github.com>
2018-02-28 17:48:42 -0500

    Add recommendation Easter Egg

--- Team_CourseAnalyses/courses/views.py
+++ Team_CourseAnalyses/courses/views.py
@@ -16,9 +16,11 @@ class IndexView(generic.ListView):
 def index(request):
     course_list = Course.objects.all()
     show_time, ongoing_course_list = get_show_time(course_list)
+    recommend_course_list = Course.objects.filter(course_id='95880')
 
     return render(request, 'courses/index.html',
-                  {'course_list': course_list, 'ongoing_course_list': ongoing_course_list, 'show_time': show_time})
+                  {'course_list': course_list, 'ongoing_course_list': ongoing_course_list, 'show_time': show_time,
+                   'recommend_course_list': recommend_course_list})
 
 
 def search(request):
@@ -37,11 +39,15 @@ def search(request):
     elif search_field == 'building':
         course_list = Course.objects.filter(bldg_room__contains=search_word)
 
-    show_time, ongoing_course_list = get_show_time(course_list)
+    recommend_course_list = Course.objects.filter(course_id='95880')
+    if course_list:
+        show_time, ongoing_course_list = get_show_time(course_list)
+    else:
+        show_time, ongoing_course_list = get_show_time(recommend_course_list)
 
     return render(request, 'courses/index.html',
                   {'course_list': course_list, 'ongoing_course_list': ongoing_course_list, 'search_field': search_field,
-                   'show_time': show_time})
+                   'show_time': show_time, 'recommend_course_list': recommend_course_list})
 
 
 def get_show_time(course_list):
@@ -52,7 +58,6 @@ def get_show_time(course_list):
     day = now.strftime('%A')[0]
     time = now.strftime('%H:%M')
     now_minutes = int(time[0:2]) * 60 + int(time[3:5])
-    print(time, ': ', now_minutes)
 
     for course in course_list:
         if 'A' not in course.section or '4' in course.section or day not in course.days:  # not a current course
