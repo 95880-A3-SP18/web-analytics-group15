2f752381068cee76c7e9b60d6a4f993a6dba6ef2
Dan Hou <31412607+doradhou@users.noreply.github.com>
2018-03-03 01:04:36 -0500

    heroku

--- Team_CourseAnalyses/Team_CourseAnalyses/wsgi.py
+++ Team_CourseAnalyses/Team_CourseAnalyses/wsgi.py
@@ -14,7 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Team_CourseAnalyses.settings")
 
 application = get_wsgi_application()
-
-# Add for Heroku
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
